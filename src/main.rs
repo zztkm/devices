299267d822caa59d266572deb105ab6f6867a216
zztkm <zztkm@tsurutatakumi.info>
2024-03-20 11:10:57 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use nokhwa::{native_api_backend, query};
+
+fn main() {
+    println!("{}", std::env::consts::OS);
+    // DirectShow をサポートしていないので、OBS Virtual Camera は検出されない
+    // 参考: https://github.com/l1npengtul/nokhwa/issues/102
+    let backend = native_api_backend().unwrap();
+    let devices = query(backend).unwrap();
+    println!("There are {} devices", devices.len());
+    for device in devices {
+        println!(
+            "device name: {}, index: {}",
+            device.human_name(),
+            device.index()
+        );
+    }
+}
